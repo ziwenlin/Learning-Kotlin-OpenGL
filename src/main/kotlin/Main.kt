253d4dcb00fb09839ded56acb4c92ec10d7d8326
ziwenlin <zw.lin1998@hotmail.com>
2023-11-28 01:24:20 +0100

    Added basic window in Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,36 @@
 *
 * */
 
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.system.MemoryUtil.NULL
+
 fun main(args: Array<String>) {
     println("Hello World!")
     println("Program arguments: ${args.joinToString()}")
+
+    val glfwInitialize = glfwInit()
+    if (glfwInitialize != true) {
+        throw Error("GLFW could not be initialized!")
+    }
+
+    val window: Long = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL)
+    if (window == NULL) {
+        glfwTerminate()
+        throw Error("Window could not be created!")
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window)
+
+    while (glfwWindowShouldClose(window) == false) {
+        // Swap front and back buffers
+        glfwSwapBuffers(window)
+
+        // Poll for and process events
+        glfwPollEvents()
+    }
+
+    glfwDestroyWindow(window)
+    glfwTerminate()
+
 }
