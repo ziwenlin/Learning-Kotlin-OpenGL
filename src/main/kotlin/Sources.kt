56f0531ac45b051a2b5a1e30c13f32c0ecdbf57e
ziwenlin <zw.lin1998@hotmail.com>
2023-11-30 00:26:54 +0100

    Added uniform vPos in vertexShaderSource

--- src/main/kotlin/Sources.kt
+++ src/main/kotlin/Sources.kt
@@ -1,26 +1,34 @@
 val vertexShaderSource = """
     #version 330 core
     layout (location = 0) in vec3 aPos;
+
+    uniform vec3 vPos;
+
     void main()
     {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        gl_Position = vec4(aPos + vPos, 1.0f);
     }
 """.trimIndent()
+
 val fragmentShaderSource = """
     #version 330 core
     out vec4 FragColor;
+
     uniform vec3 vColor;
+
     void main()
     {
-    FragColor = vec4(vColor, 1.0f);
+        FragColor = vec4(vColor, 1.0f);
     }
 """.trimIndent()
+
 val vertices = floatArrayOf(
     0.5f, 0.5f, 0.0f, // top right
     0.5f, -0.5f, 0.0f, // bottom right
     -0.5f, -0.5f, 0.0f, // bottom left
     -0.5f, 0.5f, 0.0f // top left
 )
+
 val indices = intArrayOf(
     0, 1, 3, // first triangle
     1, 2, 3 // second triangle
