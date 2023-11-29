4b4ce671b7362447fafe0c3c1fbd86ca2a6cd3be
ziwenlin <zw.lin1998@hotmail.com>
2023-11-29 20:16:05 +0100

    Removed function createElementVertexArray and createVertexBufferArray

--- src/main/kotlin/Buffers.kt
+++ src/main/kotlin/Buffers.kt
@@ -30,34 +30,3 @@ class RenderObject(vertices: FloatArray, indices: IntArray) {
         glBindVertexArray(0)
     }
 }
-
-fun createElementVertexArray(vertices: FloatArray, indices: IntArray): Int {
-    val vaoID = glGenVertexArrays()
-    val vboID = glGenBuffers()
-    val eboID = glGenBuffers()
-
-    glBindVertexArray(vaoID)
-
-    glBindBuffer(GL_ARRAY_BUFFER, vboID)
-    glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW)
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.SIZE_BYTES, 0)
-    glEnableVertexAttribArray(0)
-
-    return vaoID
-}
-
-fun createVertexBufferArray(vertices: FloatArray): Int {
-    val vaoID = glGenVertexArrays()
-    val vboID = glGenBuffers()
-
-    glBindVertexArray(vaoID)
-
-    glBindBuffer(GL_ARRAY_BUFFER, vboID)
-    glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
-
-    return vaoID
-}
