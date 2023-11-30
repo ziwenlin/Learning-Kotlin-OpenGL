1af5ca4b81f2608ec9e28b7ed192704158a4a1df
ziwenlin <zw.lin1998@hotmail.com>
2023-11-30 16:31:00 +0100

    Created Utility.kt

---
+++ src/main/kotlin/Utility.kt
@@ -0,0 +1,10 @@
+fun addValuesToArray(array: FloatArray, stride: Int, value: Float, amount: Int): FloatArray {
+    val list = arrayListOf<Float>()
+    for ((i, v) in array.withIndex()) {
+        list.add(v)
+        if (i % stride == stride - 1)
+            for (x in 1..amount)
+                list.add(value)
+    }
+    return list.toFloatArray()
+}
