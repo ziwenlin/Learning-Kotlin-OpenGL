b5aa01c60d9e07f4f6350011d816e3b40bc97fde
ziwenlin <zw.lin1998@hotmail.com>
2023-12-03 00:57:59 +0100

    Created MatrixN.kt
    Created MatrixN.kt

---
+++ src/main/kotlin/mathematics/MatrixN.kt
@@ -0,0 +1,98 @@
+package mathematics
+
+class MatrixN {
+
+    private val numRows: Int
+    private val numColumns: Int
+    private val matrixArray: FloatArray
+
+    constructor(numRows: Int, numColumns: Int) {
+        this.numRows = numRows
+        this.numColumns = numColumns
+        this.matrixArray = FloatArray(numRows * numColumns)
+    }
+
+    constructor(size: Int, identity: Float) : this(size, size) {
+        val range = size - 1
+        for (index in 0..range) {
+            set(index, index, identity)
+        }
+    }
+
+    constructor(numRows: Int, numColumns: Int, array: FloatArray) {
+        this.numRows = numRows
+        this.numColumns = numColumns
+        this.matrixArray = array
+        if (array.size != numRows * numColumns) {
+            throw Exception()
+        }
+    }
+
+    fun getArray(): FloatArray {
+        return matrixArray
+    }
+
+    operator fun get(x: Int, y: Int): Float {
+        return matrixArray[getIndex(x, y)]
+    }
+
+    operator fun set(x: Int, y: Int, value: Float) {
+        matrixArray[getIndex(x, y)] = value
+    }
+
+    operator fun plus(other: MatrixN): MatrixN {
+        val array = matrixArray.zip(other.matrixArray) { xv, yv -> xv + yv }.toFloatArray()
+        return MatrixN(numRows, numColumns, array)
+    }
+
+    operator fun minus(other: MatrixN): MatrixN {
+        val array = matrixArray.zip(other.matrixArray) { xv, yv -> xv - yv }.toFloatArray()
+        return MatrixN(numRows, numColumns, array)
+    }
+
+    operator fun times(value: Float): MatrixN {
+        return MatrixN(numRows, numColumns, matrixArray.map { it * value }.toFloatArray())
+    }
+
+    operator fun times(other: MatrixN): MatrixN {
+        val matrix = MatrixN(this.numRows, other.numColumns)
+        val sortedRows = this.getRows()
+        val sortedColumns = other.getColumns()
+        for (index in matrix.matrixArray.indices) {
+            val (row, column) = matrix.getRowColumn(index)
+            val zippedRowColumn = sortedRows[row].zip(sortedColumns[column])
+            matrix.matrixArray[index] = zippedRowColumn.map { (x, y) -> x * y }.sum()
+        }
+        return matrix
+    }
+
+    protected fun getIndex(row: Int, column: Int): Int {
+        return row * numColumns + column
+    }
+
+    protected fun getRowColumn(index: Int): Pair<Int, Int> {
+        val row = index / numColumns
+        val column = index % numColumns
+        return Pair(row, column)
+    }
+
+    fun getRows(): Array<FloatArray> {
+        val array = Array(numRows) { _ -> FloatArray(numColumns) }
+        for (row in array.indices) {
+            for (column in array[row].indices) {
+                array[row][column] = matrixArray[getIndex(row, column)]
+            }
+        }
+        return array
+    }
+
+    fun getColumns(): Array<FloatArray> {
+        val array = Array(numColumns) { _ -> FloatArray(numRows) }
+        for (column in array.indices) {
+            for (row in array[column].indices) {
+                array[column][row] = matrixArray[getIndex(row, column)]
+            }
+        }
+        return array
+    }
+}
