d04b8b54ee2597d3bb2fe56516ca98905e24470a
ziwenlin <zw.lin1998@hotmail.com>
2023-12-04 15:23:19 +0100

    Removed completed tutorial code
    Removed completed tutorial code

--- src/main/kotlin/Program.kt
+++ src/main/kotlin/Program.kt
@@ -5,28 +5,11 @@ import org.lwjgl.opengl.GL30.*
 import renderer.*
 import utility.*
 import java.nio.FloatBuffer
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
 
 val floatBuffer16: FloatBuffer = BufferUtils.createFloatBuffer(16)
 
 fun createProgram(): Pair<() -> Unit, () -> Unit> {
     // Create shader program
-    val shaderProgram = Shader("/shaders/vertex_v1.glsl", "/shaders/fragment_v1.glsl")
-    val shaderColor = shaderProgram.getUniformLocation("vColor")
-    val shaderPosition = shaderProgram.getUniformLocation("vPosition")
-
-    val shaderProgram2 = Shader("/shaders/vertex_v2.glsl", "/shaders/fragment_v2.glsl")
-    val shaderColor2 = shaderProgram2.getUniformLocation("vColor")
-    val shaderPosition2 = shaderProgram2.getUniformLocation("vPosition")
-
-    val shaderProgram3 = Shader("/shaders/vertex_v3.glsl", "/shaders/fragment_v3.glsl")
-    val shaderTexture3a = shaderProgram3.getUniformLocation("uniformTexture")
-    val shaderTexture3b = shaderProgram3.getUniformLocation("uniformTexture2")
-    val shaderPosition3 = shaderProgram3.getUniformLocation("uniformPosition")
-    val shaderVisibility3 = shaderProgram3.getUniformLocation("uniformVisibility")
-
     val shaderProgram4 = Shader("/shaders/vertex_v4.glsl", "/shaders/fragment_v4.glsl")
     val shaderTexture4a = shaderProgram4.getUniformLocation("uTexture1")
     val shaderTexture4b = shaderProgram4.getUniformLocation("uTexture2")
@@ -39,9 +22,6 @@ fun createProgram(): Pair<() -> Unit, () -> Unit> {
     // Create square render object
     val squareRenderObject = RenderObject(boxVertices, boxIndices)
     val squareTexturedRenderObject = TexturedRenderObject(boxVerticesTextured, boxIndices)
-    val triangleVerticesCorrected = addValuesToArray(triangleVertices, 3, 0.8f, 3)
-    val triangleRenderObject = SimpleRenderObject(triangleVerticesCorrected, 6)
-    val triangleRenderObject2 = SimpleRenderObject(triangleVertices2, 6)
 
     // Set the clear color and the view port
     glClearColor(0.1f, 0.3f, 0.3f, 1.0f)
@@ -60,51 +40,9 @@ fun createProgram(): Pair<() -> Unit, () -> Unit> {
         }
         // Get time since start
         val timeValue = glfwGetTime()
-        val timeRad = timeValue * PI / 2.0f
-        val timePhase = 2.0f / 3.0f * PI
-
-        // Mathematical functions values based on time
-        val sawState = if (timeValue % 4.0f < 2.0f) 1 else -1
-        val sawTooth = ((timeValue % 2.0f - 1.0f) * sawState).toFloat()
-        val sawSharp = sawTooth * sawTooth * sawTooth
-        val cosWave = cos(timeRad).toFloat()
-        val sineWave = sin(timeRad).toFloat()
-        val sineWave2 = sin(timeRad - timePhase).toFloat()
-        val sineWave3 = sin(timeRad + timePhase).toFloat()
-
-        // Mathematical transition effects
-        val sinValue = sineWave / 2.0f + 0.5f
-        val sinValue2 = sineWave2 / 2.0f + 0.5f
-        val sinValue3 = sineWave3 / 2.0f + 0.5f
-        val cosValue = cosWave / 2.0f + 0.5f
 
         // Render here
         glClear(GL_COLOR_BUFFER_BIT)
-
-        // Render the objects
-        shaderProgram.use()
-        glUniform3f(shaderPosition, 0.5f * cosWave, 0.5f * sineWave, 0f)
-        glUniform3f(shaderColor, 0.8f * sinValue, 0.8f * sinValue2, 0.8f * sinValue3)
-        squareRenderObject.draw()
-
-        // Render triangle objects
-        shaderProgram2.use()
-        glUniform3f(shaderPosition2, 0.0f, 0.5f * -sawSharp, 0.0f)
-        glUniform3f(shaderColor2, sinValue2, sinValue2, sinValue2)
-        triangleRenderObject.draw()
-        glUniform3f(shaderPosition2, 0.0f, 0.5f * sawSharp, 0.0f)
-        glUniform3f(shaderColor2, sinValue3, sinValue2, sinValue)
-        triangleRenderObject2.draw()
-
-        // Render texture objects
-        shaderProgram3.use()
-        glUniform3f(shaderPosition3, 0.5f * sineWave, 0.5f * cosWave, 0f)
-        glUniform1f(shaderVisibility3, visibilityValue * cosValue)
-        glUniform1i(shaderTexture3a, 0)
-        glUniform1i(shaderTexture3b, 1)
-        textureContainer.bind(0)
-        textureAwesome.bind(1)
-        squareTexturedRenderObject.draw()
 
         // Render texture objects
         shaderProgram4.use()
@@ -131,17 +69,12 @@ fun createProgram(): Pair<() -> Unit, () -> Unit> {
 
     // Create deconstruction program
     val destroy = { ->
-        shaderProgram.destroy()
-        shaderProgram2.destroy()
-        shaderProgram3.destroy()
+        shaderProgram4.destroy()
 
         textureContainer.destroy()
         textureAwesome.destroy()
 
-        squareRenderObject.destroy()
         squareTexturedRenderObject.destroy()
-        triangleRenderObject.destroy()
-        triangleRenderObject2.destroy()
     }
     return Pair(program, destroy)
 }
