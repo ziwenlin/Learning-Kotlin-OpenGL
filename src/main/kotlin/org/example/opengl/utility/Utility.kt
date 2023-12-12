fa78973701cec4b549b9ed19900ae3b5df9526a2
ziwenlin <zw.lin1998@hotmail.com>
2023-12-12 16:38:53 +0100

    Added function checkExistanceFile

--- src/main/kotlin/org/example/opengl/utility/Utility.kt
+++ src/main/kotlin/org/example/opengl/utility/Utility.kt
@@ -1,5 +1,7 @@
 package org.example.opengl.utility
 
+import java.io.File
+
 fun addValuesToArray(array: FloatArray, stride: Int, value: Float, amount: Int): FloatArray {
     val list = arrayListOf<Float>()
     for ((i, v) in array.withIndex()) {
@@ -9,4 +11,23 @@ fun addValuesToArray(array: FloatArray, stride: Int, value: Float, amount: Int):
                 list.add(value)
     }
     return list.toFloatArray()
+}
+
+fun checkExistanceFile(currentFile: File): Boolean {
+    val parentFile = currentFile.parentFile
+    if (parentFile.exists() == false) {
+        parentFile.mkdirs()
+        if (parentFile.exists() == false) {
+            println("Folder ${parentFile.path} does not exist. Failed to generate new folder.")
+            return false
+        }
+    }
+    if (currentFile.exists() == false) {
+        currentFile.createNewFile()
+        if (currentFile.exists() == false) {
+            println("File ${currentFile.path} does not exist. Failed to generate new file.")
+            return false
+        }
+    }
+    return true
 }
